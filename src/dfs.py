513acdc64432b2dbab67eb3fca51d07811021465
Vitor Carvalho <vitorcarvalhoml@gmail.com>
2015-10-21 23:29:04 -0300

    implemeting static method and undirected graphs

--- src/dfs.py
+++ src/dfs.py
@@ -6,11 +6,11 @@ class DFS(object):
     def __init__(self):
         pass
 
-    def dfs(self, start, graph):
+    def dfs(self, graph, start):
         visited, stack = set(), [start]
         while stack:
             node = stack.pop()
             if node not in visited:
                 visited.add(node)
-                stack.extend(self.graph[node] - visited)
+                stack.extend(graph[node] - visited)
         return visited
--- src/graph.py
+++ src/graph.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from graphviz import Digraph
+from graphviz import Graph as G
 import numpy.random as nprnd
 
 class Graph(object):
@@ -18,10 +18,19 @@ class Graph(object):
             children_nodes = [k for k in children_nodes if k > node]
             self.graph[node] = list(set(children_nodes))
 
+    def static(self):
+        self.graph[1] = set([2, 3])
+        self.graph[2] = set([1, 4, 5])
+        self.graph[3] = set([1, 6])
+        self.graph[4] = set([2])
+        self.graph[5] = set([2, 6])
+        self.graph[6] = set([3, 5])
+
     def render(self):
-        u = Digraph('graph', filename='example', format='png')
+        u = G('graph', filename='example.gv', format='png')
 
         for key, values in self.graph.iteritems():
+            u.node(str(key))
             print "chave:", key,
             print "valores:",
             for v in values:
