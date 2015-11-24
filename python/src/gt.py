e9c4020b2daa8cbc452c19fc6e29e4a694b671e8
Vitor Carvalho <vitorcarvalhoml@gmail.com>
2015-11-24 14:54:54 -0300

    creating graphs with labels

--- python/src/gt.py
+++ python/src/gt.py
@@ -7,8 +7,8 @@ gt_generation = graph_tool.generation
 gt_draw = graph_tool.draw
 gt_search = graph_tool.search
 
-print graph_tool.show_config()
-graph_tool.openmp_set_num_threads(1)
+# print graph_tool.show_config()
+# graph_tool.openmp_set_num_threads(1)
 
 def sample_k(max):
   accept = False
@@ -44,12 +44,21 @@ class VisitorExample(gt_search.DFSVisitor):
 ##### generating graph #####
 p = scipy.stats.poisson
 tic=timeit.default_timer()
-g = gt_generation.random_graph(100, lambda: (sample_k(19), sample_k(19)),
+g = gt_generation.random_graph(100000, lambda: (sample_k(19), sample_k(19)),
     model="probabilistic",
     vertex_corr=lambda a,b: (p.pmf(a[0], b[1]) *p.pmf(a[1], 20 - b[0])),
     n_iter=100)
 toc = timeit.default_timer()
 print "graph generate in: {0} seconds".format(toc - tic)
+
+##### applying labels #####
+label = g.new_vertex_property("string")
+g.vp.label = label
+tic=timeit.default_timer()
+for v in g.vertices():
+    g.vp.label[v] = g.vertex_index[v]
+toc = timeit.default_timer()
+print "applying labels took {0} seconds".format(toc - tic)
 
 ##### loading graph #####
 # tic=timeit.default_timer()
@@ -64,9 +73,9 @@ print "graph generate in: {0} seconds".format(toc - tic)
 # for k in xrange(10):
 # gt_search.bfs_search(g, g.vertex(0), BFSVisitor())
 # toc = timeit.default_timer()
-print "graph covered in: {0} seconds".format(toc - tic)
+# print "graph covered in: {0} seconds".format(toc - tic)
 
 # drawing graph
-gt_draw.graph_draw(g, output="graph-100.svg")
+# gt_draw.graph_draw(g, output="graph-100k.svg")
 # saving grpahs into gml format
-g.save('graph-100.gml')
+g.save('graph-100k.gml')
